d4da62a28f03d816cc94aeae0884e11d72baa4ca
vic1707 <28602203+vic1707@users.noreply.github.com>
2026-04-04 12:59:42 +0200

    clippy + add another test

--- tests/schema_attrs.rs
+++ tests/schema_attrs.rs
@@ -23,13 +23,19 @@ struct SchemaAttrOnField {
 #[derive(Serialize, Deserialize, Schema, PartialEq, Eq, Debug)]
 enum SchemaAttrOnVariant {
     #[schema(as(u8))]
-    Foo { foo: u16 },
+    Foo { toto: u16 },
 }
 
 #[derive(Serialize, Deserialize, Schema, PartialEq, Eq, Debug)]
 enum SchemaAttrOnTupleVariant {
     #[schema(as(u8))]
     Foo(u16, bool),
+}
+
+#[derive(Serialize, Deserialize, Schema, PartialEq, Eq, Debug)]
+enum SchemaAttrOnTupleVariantField {
+    
+    Foo(u16, #[schema(as(u8))] bool),
 }
 
 struct NotSchema;
@@ -50,8 +56,9 @@ struct SchemaAttrOnBound<T>(#[schema(as(u8))] T);
 #[case::transparent((Transparent { foo: 0, bar: 0 }, TypeSchema::U8))]
 #[case::schema_attr_item((SchemaAttrOnItem { foo_bar: 0 }, TypeSchema::U8))]
 #[case::schema_attr_field((SchemaAttrOnField { foo: 0 }, TypeSchema::Struct { name: "SchemaAttrOnField", fields: &[&FieldSchema { name: "foo", ty: &TypeSchema::U16 }] as &[&_] }))]
-#[case::schema_attr_variant((SchemaAttrOnVariant::Foo { foo: 0 }, TypeSchema::Enum { name: "SchemaAttrOnVariant", variants: &[&VariantSchema::NewType { name: "Foo", discriminant: 0, field: &TypeSchema::U8 }] as &[&_] }))]
+#[case::schema_attr_variant((SchemaAttrOnVariant::Foo { toto: 0 }, TypeSchema::Enum { name: "SchemaAttrOnVariant", variants: &[&VariantSchema::NewType { name: "Foo", discriminant: 0, field: &TypeSchema::U8 }] as &[&_] }))]
 #[case::schema_attr_tuple_variant((SchemaAttrOnTupleVariant::Foo(0, false), TypeSchema::Enum { name: "SchemaAttrOnTupleVariant", variants: &[&VariantSchema::NewType { name: "Foo", discriminant: 0, field: &TypeSchema::U8 }] as &[&_] }))]
+#[case::schema_attr_tuple_variant((SchemaAttrOnTupleVariantField::Foo(0, false), TypeSchema::Enum { name: "SchemaAttrOnTupleVariantField", variants: &[&VariantSchema::Tuple { name: "Foo", discriminant: 0, fields: &[&TypeSchema::U16, &TypeSchema::U8] as &[&_] }] as &[&_] }))]
 #[case::schema_attr_bound((SchemaAttrOnBound(NotSchema), TypeSchema::NewTypeStruct { name: "SchemaAttrOnBound", field: &TypeSchema::U8 }))]
 fn schemas<T: Schema>(#[case] (_ty, expected_schema): (T, StaticSchema)) {
     assert_eq!(T::SCHEMA, &expected_schema);
