0592ee6a2d41128fbe67cdf28933d8f4002aed46
vic1707 <28602203+vic1707@users.noreply.github.com>
2026-05-12 20:02:41 +0200

    reorder cargo.toml + update

--- src/schema/primitive_impls/heapless_impls.rs
+++ src/schema/primitive_impls/heapless_impls.rs
@@ -1,10 +1,7 @@
 use crate::{Schema, StaticSchema, schema};
-use ::{
-    hash32::BuildHasherDefault,
-    heapless::{
-        BinaryHeap, Deque, HistoryBuf, IndexMap, IndexSet, LinearMap, String, Vec,
-        binary_heap::Kind as BinaryHeapKind,
-    },
+use ::heapless::{
+    BinaryHeap, Deque, HistoryBuf, IndexMap, IndexSet, LinearMap, String, Vec,
+    binary_heap::Kind as BinaryHeapKind,
 };
 
 impl<const N: usize> Schema for String<N> {
@@ -27,14 +24,14 @@ impl<T: Schema, const N: usize> Schema for HistoryBuf<T, N> {
     const SCHEMA: &'static StaticSchema = &schema::Schema::Slice { element: T::SCHEMA };
 }
 
-impl<K: Schema, V: Schema, S, const N: usize> Schema for IndexMap<K, V, BuildHasherDefault<S>, N> {
+impl<K: Schema, V: Schema, S, const N: usize> Schema for IndexMap<K, V, S, N> {
     const SCHEMA: &'static StaticSchema = &schema::Schema::Map {
         key: K::SCHEMA,
         value: V::SCHEMA,
     };
 }
 
-impl<T: Schema, S, const N: usize> Schema for IndexSet<T, BuildHasherDefault<S>, N> {
+impl<T: Schema, S, const N: usize> Schema for IndexSet<T, S, N> {
     const SCHEMA: &'static StaticSchema = &schema::Schema::Slice { element: T::SCHEMA };
 }
 
